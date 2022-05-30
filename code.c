947efa22098a76672d3d12c3f8f6e06699622cc4
Gabriel Araujo <46913108+Scientistg@users.noreply.github.com>
2022-05-29 21:36:56 -0300

    Example Code

--- code.c
+++ code.c
@@ -1,7 +1,26 @@
-
-#include <stdio.h>
-
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+ 
 int main(int argc, char *argv[]){
-    printf("Hello world!\n");
-    return 0;
+    int fd;
+ 
+    // export GPIO
+    fd = open("/sys/class/gpio/export", O_WRONLY);
+    write(fd, "89", 3);
+    close(fd);
+ 
+    // Configure as output
+    fd = open("/sys/class/gpio/gpio89/direction", O_WRONLY);
+    write(fd, "out", 3);
+    close(fd);
+ 
+    // Blink GPIO once
+    fd = open("/sys/class/gpio/gpio89/value", O_WRONLY | O_SYNC);
+    write(fd, "0", 1);
+    usleep(1000000);
+    write(fd, "1", 1);
+    close(fd);
+ 
+    return EXIT_SUCCESS;
 }
