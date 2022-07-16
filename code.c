3093be80330dda81678c74d6da3d2585630ac907
Gabriel Araujo <46913108+Scientistg@users.noreply.github.com>
2022-07-16 11:48:43 -0300

    Autostart on boot added and code renamed

--- code.c
+++ code.c
@@ -1,31 +1,92 @@
 #include <stdlib.h>
-#include <fcntl.h>
+#include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+#include <fcntl.h>
+#include <stdbool.h>
 
 #define MAX 100
 
+bool jogoTerminou = false;
+
+int fd;
+char path0[] = "/sys/class/gpio/export";
+char path1[] = "/sys/class/gpio/gpio";
+char path2[] = "/direction";
+char path3[] = "/value";
+char buffer[MAX];
+char ports[5][3] = {"46","47","48","88","89"};
+
+
+void writePort(char *port, char *value){
+    strcat(strcpy(buffer, path1), port);
+    strcat(strcpy(buffer, buffer), path3);
+    fd = open(buffer, O_WRONLY);
+    write(fd, value, 1);
+    close(fd);
+}
+
+bool readPort(char *port){
+    char lido[3];
+    strcat(strcpy(buffer, path1), port);
+    strcat(strcpy(buffer, buffer), path3);    
+    fd = open(buffer, O_RDONLY);
+    int a = read(fd, lido, 2);
+    close(fd);
+    if(lido[0] == 48) return true;
+    else return false;
+}
+
+
+void escritaLed(int ledNum, char *value){
+    char gpio[3];
+    switch (ledNum){
+    case 1:
+        writePort(ports[3],value);
+        break;
+    
+    case 2:
+        writePort(ports[4],value); 
+        break;
+
+    default:
+        writePort(ports[3],"0");
+        writePort(ports[4],"0");
+        break;
+    }
+}
+
+void resetGame(){
+    escritaLed(1,"1");
+    escritaLed(2,"1");
+    jogoTerminou = false;
+}
 
 
  
 int main(int argc, char *argv[]){
-    int fd;
-    char path1[] = "/sys/class/gpio/gpio";
-    char path2[] = "/direction";
-    char buffer[MAX];
-    int ports[5][3] = {"73","75","77","88","89"};
+    
 
     // export GPIO
     int i;
-    fd = open("/sys/class/gpio/export", O_WRONLY);
+    fd = open(path0, O_WRONLY);
     for(i = 0; i < 5; i++){
-        write(fd, ports[i], 3);
+        strcat(strcpy(buffer, ports[i]), "\n");
+        write(fd, buffer, 3);
     }
     close(fd);
 
 
+    // Configure as input
+    for(i = 0; i < 3; i++){
+        strcat(strcpy(buffer, path1), ports[i]);
+        strcat(strcpy(buffer, buffer), path2);    
+        fd = open(buffer, O_WRONLY);
+        write(fd, "in", 3);
+        close(fd);
+    }
+
     // Configure as output
-    for(i = 0; i < 5; i++){
+    for(i = 3; i < 5; i++){
         strcat(strcpy(buffer, path1), ports[i]);
         strcat(strcpy(buffer, buffer), path2);    
         fd = open(buffer, O_WRONLY);
@@ -33,22 +94,27 @@ int main(int argc, char *argv[]){
         close(fd);
     }
 
-    // Read port
-    while(true){
-        fd = open("/sys/class/gpio/gpio89/value", O_RDONLY);
-        read(fd, "0", 1);
-        usleep(1000000);
-        write(fd, "1", 1);
-        close(fd);
- 
-    }
 
-    // // Blink GPIO once
-    // fd = open("/sys/class/gpio/gpio89/value", O_WRONLY | O_SYNC);
-    // write(fd, "0", 1);
-    // usleep(1000000);
-    // write(fd, "1", 1);
-    // close(fd);
+
+    resetGame();
+    while(true){
+        printf("\t\t%d\t%d\t\t\n", !jogoTerminou, readPort(ports[0]));
+        usleep(1000);
+        if(readPort(ports[0]) && !jogoTerminou){
+            escritaLed(1, "0");
+            printf("Game Ganhou 1");
+            jogoTerminou = true;
+        } else if(readPort(ports[1]) && !jogoTerminou){
+            escritaLed(2, "0");
+            printf("Game Ganhou 2");
+            jogoTerminou = true;
+        }
+        
+        if(readPort(ports[2])){
+            resetGame();
+            printf("Game resetado");
+        }
+    }
  
     return EXIT_SUCCESS;
 }
